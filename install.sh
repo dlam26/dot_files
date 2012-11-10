3cb3ba41e8b3daeade455aee5bc9a704ce6ab088
David Lam <d@dlam.me>
2012-11-10 11:19:09 -0800

    oops, install.sh bug :O

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@
 FILES="bashrc gitignore hgrc screenrc tmux.conf vimrc"
 DOT_FILES=~/dot_files
 
-ln -ifsv $HOME/.hgrc $DOT_FILES/.hgrc
-
 for file in $FILES; do
     ln -ifsv $DOT_FILES/.$file ~/.$file
 done
