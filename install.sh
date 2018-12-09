0afb6e3fdc8a26558b2ed66bd9652a0378be20ff
David Lam <david@miro.one>
2018-12-08 19:03:39 -0800

    add .ackrc to FILES install.sh should put in

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #installs dot_files stuff to $HOME !
 
-FILES="bashrc ctags gitignore hgrc screenrc tmux.conf vimrc"
+FILES="ackrc bashrc ctags gitignore hgrc screenrc tmux.conf vimrc"
 DOT_FILES=~/dot_files
 
 for file in $FILES; do
